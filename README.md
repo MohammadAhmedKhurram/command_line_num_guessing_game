067e1563a7e0a89710543f1666a718250d687619
MohammadAhmedKhurram <162457856+MohammadAhmedKhurram@users.noreply.github.com>
2024-09-03 16:38:29 +0500

    --

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# command_line_num_guessing_game
-It's a number guessing game you play by typing commands in a terminal window.
+# command_line_num_guessing_gamm
+
+A TypeScript-based command-line number guessing game using Node.js, Inquirer, and Chalk. Players choose a difficulty level and try to guess a randomly generated number within the specified range.
+
+# About
+
+This project was developed as an assignment for the first quarter of the Governor Sindh IT Initiative Program. It will be showcased on my GitHub and npm profiles.
--- main.ts
+++ main.ts
@@ -1,82 +1,67 @@
-#! /usr/bin/env node
+#!/usr/bin/env node
 
 import inquirer from "inquirer";
 import chalk from "chalk";
 
-const randomNumber = Math.floor(Math.random() * 5 + 1);
-const randomNumber2 = Math.floor(Math.random() * 8 + 1);
-const randomNumber3 = Math.floor(Math.random() * 10 + 1);
+console.log(chalk.blue("Welcome to the hidden world!"));
 
+const input1 = await inquirer.prompt({
+    type: "list",
+    message: "Choose a difficulty level...",
+    name: "difficultyLevel",
+    choices: ["Simple", "Standard", "Difficult"]
+});
 
-console.log(chalk.yellow("Welcome to a world of hidden numbers!"));
+let condition = true;
+while (condition) {
+    if (input1.difficultyLevel == "Simple") {
+        const randomNumber1: number = Math.floor(Math.random() * 5 + 1);
+        const input2 = await inquirer.prompt({
+            type: "number",
+            message: "Your challenge is to guess a number from 1 to 5",
+            name: "easy"
+        });
 
-const difficulty = await inquirer.prompt([
-    {
-        type: "list",
-        message: "Pls choose a difficulty level!",
-        name: "level",
-        choices: ["Easy", "Normal", "Hard"],
-    }
-]);
-
-if (difficulty.level === "Easy"){
-    const input = await inquirer.prompt([
-        {
-          type: "input",
-          message: "Your challenge is to guess a number from 1 through 5.",
-          name: "easy",
-          filter: (val) => Number(val) 
-
+        if (input2.easy === randomNumber1) {
+            console.log(chalk.green("Correct guess!"));
+            condition = false;
+        } else {
+            console.log(chalk.red("Wrong guess!"));
+            console.log(chalk.yellow("The number was"), chalk.bold.greenBright(`${randomNumber1} .`));
         }
-    ]);
-
-    if (input.easy === randomNumber){
-        console.log(chalk.green("Perfect guess! You've chosen the right number."));
     }
-    else {
-        console.log(chalk.red("Your guess missed the mark. Try Again!"));
-        console.log(chalk.green(`The number was ${randomNumber}`));
 
+    if (input1.difficultyLevel == "Standard") {
+        const randomNumber2: number = Math.floor(Math.random() * 8 + 1);
+        const input3 = await inquirer.prompt({
+            type: "number",
+            message: "Your challenge is to guess a number from 1 to 8",
+            name: "normal"
+        });
+
+        if (input3.normal === randomNumber2) {
+            console.log(chalk.green("Correct guess!"));
+            condition = false;
+        } else {
+            console.log(chalk.red("Wrong guess"));
+            console.log(chalk.yellow("The number was"), chalk.bold.greenBright(`${randomNumber2} .`));
+        }
+    }
+
+    if (input1.difficultyLevel == "Difficult") {
+        const randomNumber3: number = Math.floor(Math.random() * 10 + 1);
+        const input4 = await inquirer.prompt({
+            type: "number",
+            message: "Your challenge is to guess a number from 1 to 10",
+            name: "hard"
+        });
+
+        if (input4.hard === randomNumber3) {
+            console.log(chalk.green("Correct guess!"));
+            condition = false;
+        } else {
+            console.log(chalk.red("Wrong guess"));
+            console.log(chalk.yellow("The number was"), chalk.bold.greenBright(`${randomNumber3} .`));
+        }
     }
 }
-
-if (difficulty.level === "Normal"){
-    const input2 = await inquirer.prompt([
-        {
-          type: "input",
-          message: "Your challenge is to guess a number from 1 through 8.",
-          name: "normal", 
-          filter: (val) => Number(val) 
-
-        }
-    ]);
-
-    if (input2.normal === randomNumber2){
-        console.log(chalk.green("Perfect guess! You've chosen the right number."));
-    }
-    else {
-        console.log(chalk.red("Your guess missed the mark. Try Again!"));
-        console.log(chalk.green(`The number was ${randomNumber2}`));
-    }
-}
-
-if (difficulty.level === "Hard"){
-    const input3 = await inquirer.prompt([
-        {
-          type: "input",
-          message: "Your challenge is to guess a number from 1 through 10.",
-          name: "hard",
-          filter: (val) => Number(val) 
-        }
-    ]);
-
-    if (input3.hard === randomNumber3){
-        console.log(chalk.green("Perfect guess! You've chosen the right number."));
-    }
-    else {
-        console.log(chalk.red("Your guess missed the mark. Try Again!"));
-        console.log(chalk.green(`The number was ${randomNumber3}`));
-
-    }
-}
-
